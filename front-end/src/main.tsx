89bd891e860a7777beb9c924a78861d59067c41f
Vu Duy Tuan <contact.duytuan@gmail.com>
2023-06-03 20:41:05 +0700

    update .env

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
